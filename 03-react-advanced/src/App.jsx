a3537e31784124fb22fda838124807930d3f599d
Maya Swami <mswami6367@gmail.com>
2026-07-27 17:23:17 +0530

    feat(context-api): create context, provider, and consume static data with useContext.

--- 03-react-advanced/src/App.jsx
+++ 03-react-advanced/src/App.jsx
@@ -1,3 +1,5 @@
+import UserContext from "./context/UserContext";
+
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
@@ -9,9 +11,17 @@ import UseMemoDemo from "./pages/UseMemoDemo";
 import UseRefDemo from "./pages/UseRefDemo";
 
 function App() {
+
+  const user = {
+    name: "Maya",
+    age: 22,
+    city: "Jaipur",
+  };
+  
   return (
     <>
       <Navbar />
+      <UserContext.Provider value={user}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/context-api" element={<ContextDemo />} />
@@ -21,6 +31,7 @@ function App() {
         <Route path="/use-memo" element={<UseMemoDemo />} />
         <Route path="/use-ref" element={<UseRefDemo />} />
       </Routes>
+      </UserContext.Provider>
     </>
   );
 }
--- 03-react-advanced/src/pages/ContextDemo.jsx
+++ 03-react-advanced/src/pages/ContextDemo.jsx
@@ -1,5 +1,17 @@
+import { useContext } from "react";
+import UserContext from "../context/UserContext";
+
 function ContextDemo() {
-  return <h1>Context API Demo</h1>;
+  const user = useContext(UserContext);
+
+  return (
+    <div>
+      <h1>Context API Demo</h1>
+      <h2>Name: {user.name}</h2>
+      <h2>Age: {user.age}</h2>
+      <h2>City: {user.city}</h2>
+    </div>
+  );
 }
 
 export default ContextDemo;
